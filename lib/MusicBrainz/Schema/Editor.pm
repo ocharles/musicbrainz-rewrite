c1e358714e9a14c45bb7f35753756a1653596480
Oliver Charles <ollie@oliver-charless-macbook.local>
2009-01-01 22:46:32 +0000

    Added user preferences support.

--- lib/MusicBrainz/Schema/Editor.pm
+++ lib/MusicBrainz/Schema/Editor.pm
@@ -10,4 +10,16 @@ __PACKAGE__->table('moderator');
 __PACKAGE__->add_columns(qw/id name password email/);
 __PACKAGE__->set_primary_key('id');
 
+__PACKAGE__->has_many(preferences => 'MusicBrainz::Schema::UserPreference', 'moderator');
+
+sub preference {
+    my ($self, $key) = @_;
+ 
+    my $value = $self->preferences->single({ name => $key });
+    $value = defined $value ? $value 
+           : MusicBrainz::Schema::UserPreference->default_preference($key);
+
+    return $value;
+}
+
 1;
---
+++ lib/MusicBrainz/Schema/UserPreference.pm
@@ -0,0 +1,26 @@
+package MusicBrainz::Schema::UserPreference;
+
+use strict;
+use warnings;
+
+use base 'DBIx::Class';
+
+__PACKAGE__->load_components(qw/ PK::Auto Core /);
+__PACKAGE__->table('moderator_preference');
+__PACKAGE__->add_columns(qw/ id moderator name value /);
+__PACKAGE__->set_primary_key('id');
+
+__PACKAGE__->belongs_to(editor => 'MusicBrainz::Schema::Editor', 'moderator');
+
+use Readonly;
+Readonly::Hash our %DEFAULT_PREFERENCES => (
+    subscriptions_public => 1,
+);
+
+sub default_preference {
+    my ($class, $key) = @_;
+    
+    return $DEFAULT_PREFERENCES{$key};
+}
+
+1;
