e5e4aafc8289e0d3ba7097fb0dd45839b4bde5ba
Oliver Charles <ollie@oliver-charless-macbook.local>
2009-01-01 16:46:09 +0000

    Improvements to the artist header
    Now shows date correctly, along with data quality

--- lib/MusicBrainz/Schema/Artist.pm
+++ lib/MusicBrainz/Schema/Artist.pm
@@ -45,4 +45,24 @@ sub end_date_label
     return $artist_type_names{$self->type}->[2];
 }
 
+Readonly::Scalar our $QUALITY_UNKNOWN        => -1;
+Readonly::Scalar our $QUALITY_UNKNOWN_MAPPED => 1;
+Readonly::Scalar our $QUALITY_LOW            => 0;
+Readonly::Scalar our $QUALITY_NORMAL         => 1;
+Readonly::Scalar our $QUALITY_HIGH           => 2;
+
+Readonly::Hash our %QUALITY_NAMES => (
+    $QUALITY_LOW    => 'low',
+    $QUALITY_NORMAL => 'default',
+    $QUALITY_HIGH   => 'high',
+);
+
+sub quality_name {
+    my $self = shift;
+
+    my $quality = $self->quality;
+    $quality = $QUALITY_UNKNOWN_MAPPED if $quality == $QUALITY_UNKNOWN;
+    return $QUALITY_NAMES{$quality};
+}
+
 1;
